2c941143ee112f4846cefb184d5beac98485dd88
dogbertai <dogbertai@users.noreply.github.com>
2020-06-10 02:38:36 -0400

    Add poll to wait for install to shutdown

--- coreos-prov.ps1
+++ coreos-prov.ps1
@@ -1,5 +1,19 @@
 
-function New-InstallVM {
+function New-EmbedMedia {
+    param (
+        ${VMName},
+        ${InstallMedia}
+    )
+    venv\Scripts\python.exe generatefcc.py ${VMName}
+    docker run --rm -v ${PWD}:/work quay.io/coreos/fcct:release --pretty --strict -o /work/mycoreos.ign /work/mycoreos.fcc
+    venv\Scripts\python.exe generatebootstrap.py
+    docker run --rm -v ${PWD}:/work quay.io/coreos/fcct:release --pretty --strict -o /work/bootstrap.ign /work/bootstrap.fcc
+    ${EmbedMedia} = "${PWD}\fedora-coreos-embed.iso"
+    Copy-Item -Path ${InstallMedia} -Destination ${EmbedMedia} -Force
+    docker run --rm -v ${PWD}:/work quay.io/coreos/coreos-installer:release iso embed --config /work/bootstrap.ign --force /work/fedora-coreos-embed.iso
+    return ${EmbedMedia}
+}
+function New-NodeVM {
     param (
         ${VMName},
         ${Switch},
@@ -11,31 +25,21 @@ function New-InstallVM {
     Add-VMDvdDrive -VMName ${VMName} -ControllerNumber 1 -ControllerLocation 0 -Path ${InstallMedia}
     ${DVDDrive} = Get-VMDvdDrive -VMName ${VMName}
     Set-VMFirmware -VMName ${VMName} -FirstBootDevice ${DVDDrive} -EnableSecureBoot Off
-    #Set-VMDvdDrive -VMName ${VMName} -Path ${InstallMedia}
 }
 function Install-OSToHD {
-    try {
-        Start-VM -Name ${VMName}
-        VMConnect.exe localhost ${VMName}
-        #curl -LO http://windforce.internal.dogbertai.net:8080/mycoreos.ign
-        #sudo coreos-installer install /dev/sda --ignition-file mycoreos.ign
-        Read-Host -Prompt 'Press ENTER to continue...'
-    }
-    finally {
-        Stop-VM -Name ${VMName}
+    Start-VM -Name ${VMName}
+    Start-Sleep 5
+    ${VM} = Get-VM -Name ${VMName}
+    while(${VM}.State -eq 'Running') {
+        Start-Sleep 5
     }
 }
 ${VMName} = 'CoreOS'
 ${Switch} = 'External Switch'
 ${InstallMedia} = "C:\Users\dogbe\Downloads\fedora-coreos-31.20200505.3.0-live.x86_64.iso"
-venv\Scripts\python.exe generatefcc.py ${VMName}
-docker run --rm -v ${PWD}:/work quay.io/coreos/fcct:release --pretty --strict -o /work/mycoreos.ign /work/mycoreos.fcc
-venv\Scripts\python.exe generatebootstrap.py
-docker run --rm -v ${PWD}:/work quay.io/coreos/fcct:release --pretty --strict -o /work/bootstrap.ign /work/bootstrap.fcc
-${EmbedMedia} = "${PWD}\fedora-coreos-embed.iso"
-Copy-Item -Path ${InstallMedia} -Destination ${EmbedMedia} -Force
-docker run --rm -v ${PWD}:/work quay.io/coreos/coreos-installer:release iso embed --config /work/bootstrap.ign --force /work/fedora-coreos-embed.iso
-New-InstallVM -VMName ${VMName} -Switch ${Switch} -InstallMedia ${EmbedMedia}
+${EmbedMedia} = New-EmbedMedia -VMName ${VMName} -InstallMedia ${InstallMedia}
+New-NodeVM -VMName ${VMName} -Switch ${Switch} -InstallMedia ${EmbedMedia}
+Start-Sleep 5
 Install-OSToHD -VMName ${VMName}
 Set-VMDvdDrive -VMName ${VMName} -Path $null
 ${DiskDrive} = Get-VMHardDiskDrive -VMName ${VMName}
